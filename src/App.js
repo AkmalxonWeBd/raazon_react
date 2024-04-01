a9c6dd9243a5e2783113f13b4848ea7127c64e1e
AkmalxonWeBd <akmalxonizzatullayev770@gmail.com>
2024-04-01 17:31:09 +0500

    ascd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RamazonTaqvimi from './ramazon';
+
+function App() {
+  return (
+    <div className="App">
+      <RamazonTaqvimi/>
+    </div>
+  );
+}
+
+export default App;
