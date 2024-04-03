49c3aa02e1d2fdf90cf217656f2e9a16c3619cf4
AkmalxonWeBd <akmalxonizzatullayev770@gmail.com>
2024-04-03 18:17:22 +0500

    salom

--- src/ramazon.jsx
+++ src/ramazon.jsx
@@ -1,59 +1,66 @@
 import React, { useState, useEffect } from 'react';
 
 function RamadanCalendar() {
-    const [days, setDays] = useState([
-        "11 mart", "12 mart", "13 mart", "14 mart", "15 mart", "16 mart", "17 mart", "18 mart", "19 mart", "20 mart",
-        "21 mart", "22 mart", "23 mart", "24 mart", "25 mart", "26 mart", "27 mart", "28 mart", "29 mart", "30 mart",
-        "31 mart", "1 aprel", "2 aprel", "3 aprel", "4 aprel", "5 aprel", "6 aprel", "7 aprel", "8 aprel", "9 aprel"
-    ]);
-
-    const [saharlikVaqtlar, setSaharlikVaqtlar] = useState([
-        "05:13", "05:12", "05:10", "05:08", "05:07", "05:05", "05:03", "05:01", "05:00", "04:58",
-        "04:56", "04:54", "04:53", "04:51", "04:49", "04:47", "04:45", "04:44", "04:42", "04:40",
-        "04:38", "04:36", "04:34", "04:33", "04:31", "04:29", "04:27", "04:25", "04:23", "04:22"
-    ]);
-
-    const [iftorlikVaqtlar, setIftorlikVaqtlar] = useState([
-        "18:19", "18:20", "18:21", "18:22", "18:23", "18:24", "18:25", "18:27", "18:28", "18:29",
-        "18:30", "18:31", "18:32", "18:33", "18:34", "18:35", "18:36", "18:37", "18:38", "18:39",
-        "18:40", "18:42", "18:43", "18:44", "18:45", "18:46", "18:47", "18:48", "18:49", "18:50"
-    ]);
-
-    const [remainingTimes, setRemainingTimes] = useState(Array(days.length).fill(""));
-
-    const calculateRemainingTime = (saharlik, iftorlik, dayIndex) => {
-        const currentTime = new Date();
-        const saharlikTime = new Date(`2024-03-${dayIndex + 11}T${saharlik}`);
-        const iftorlikTime = new Date(`2024-03-${dayIndex + 11}T${iftorlik}`);
-        let remainingTimeSeconds;
-
-        if (currentTime < saharlikTime) {
-            remainingTimeSeconds = Math.floor((saharlikTime - currentTime) / 1000);
-        } else if (currentTime < iftorlikTime) {
-            remainingTimeSeconds = Math.floor((iftorlikTime - currentTime) / 1000);
-        } else {
-            return "Utib ketdi";
-        }
-
-        const daysLeft = Math.floor(remainingTimeSeconds / (24 * 3600));
-        const hoursLeft = Math.floor((remainingTimeSeconds % (24 * 3600)) / 3600);
-        const minutesLeft = Math.floor((remainingTimeSeconds % 3600) / 60);
-        const secondsLeft = Math.floor(remainingTimeSeconds % 60);
-
-        return `${daysLeft} kun, ${hoursLeft} soat, ${minutesLeft} daqiqa, ${secondsLeft} soniya`;
-    };
-
+    const [currentDate, setCurrentDate] = useState(new Date());
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            const updatedRemainingTimes = days.map((day, index) => {
-                return calculateRemainingTime(saharlikVaqtlar[index], iftorlikVaqtlar[index], index);
-            });
-            setRemainingTimes(updatedRemainingTimes);
+        const timer = setInterval(() => {
+            setCurrentDate(new Date());
         }, 1000);
 
-        return () => clearInterval(interval);
-    }, []); // Empty dependency array to run effect only once
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    const martDays = [
+        "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+        "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+        "31"
+    ];
+    const aprilDays = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    ];
+
+    const martSaharlikVaqtlar = [
+        "05:13", "05:12", "05:10", "05:08", "05:07", "05:05", "05:03", "05:01", "05:00", "04:58",
+        "04:56", "04:54", "04:53", "04:51", "04:49", "04:47", "04:45", "04:44", "04:42", "04:40",
+        "04:38",
+    ];
+    const aprilSaharlikVaqtlar = [
+        "04:36", "04:34", "04:33", "04:31", "04:29", "04:27", "04:25", "04:23", "04:22"
+    ];
+
+    const martIftorlikVaqtlar = [
+        "18:19", "18:20", "18:21", "18:22", "18:23", "18:24", "18:25", "18:27", "18:28", "18:29",
+        "18:30", "18:31", "18:32", "18:33", "18:34", "18:35", "18:36", "18:37", "18:38", "18:39",
+        "18:40",
+    ];
+    const aprilIftorlikVaqtlar = [
+        "18:42", "18:43", "18:44", "18:45", "18:46", "18:47", "18:48", "18:49", "18:50"
+    ];
+
+    // Qolgan vaqtni hisoblash funksiyasi
+    const calculateRemainingTime = (targetTime, iftorlikVaqti) => {
+        const diffInMs = targetTime.getTime() - currentDate.getTime();
+        if (diffInMs <= 0) {
+            if (currentDate.getTime() > iftorlikVaqti.getTime()) {
+                return "Utib ketdi";
+            } else {
+                const diff = iftorlikVaqti.getTime() - currentDate.getTime();
+                const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+                const minutes = Math.floor((diff / (1000 * 60)) % 60);
+                const seconds = Math.floor((diff / 1000) % 60);
+                return `Saharlik o'tib ketdi: iftorlikga ${hours} soat,${minutes} daqiqa,${seconds} soniya`;
+            }
+        } else {
+            const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((diffInMs / (1000 * 60 * 60)) % 24);
+            const minutes = Math.floor((diffInMs / (1000 * 60)) % 60);
+            const seconds = Math.floor((diffInMs / 1000) % 60);
+            return `${days} kun, ${hours} soat, ${minutes} daqiqa, ${seconds} soniya`;
+        }
+    };
 
     return (
         <div className="container">
@@ -69,12 +76,20 @@ function RamadanCalendar() {
                         </tr>
                     </thead>
                     <tbody>
-                        {days.map((day, index) => (
-                            <tr key={index} className={index === 4 ? 'odd' : index % 2 === 0 ? 'even' : 'odd'}>
-                                <td>{day}</td>
-                                <td>{saharlikVaqtlar[index]}</td>
-                                <td>{iftorlikVaqtlar[index]}</td>
-                                <td>{remainingTimes[index]}</td>
+                        {martDays.map((day, index) => (
+                            <tr key={day}>
+                                <td>{`${day} mart`}</td>
+                                <td>{martSaharlikVaqtlar[index]}</td>
+                                <td>{martIftorlikVaqtlar[index]}</td>
+                                <td>{calculateRemainingTime(new Date(`March ${day}, ${currentDate.getFullYear()} ${martSaharlikVaqtlar[index]}`), new Date(`March ${day}, ${currentDate.getFullYear()} ${martIftorlikVaqtlar[index]}`))}</td>
+                            </tr>
+                        ))}
+                        {aprilDays.map((day, index) => (
+                            <tr key={day}>
+                                <td>{`${day} Aprel`}</td>
+                                <td>{aprilSaharlikVaqtlar[index]}</td>
+                                <td>{aprilIftorlikVaqtlar[index]}</td>
+                                <td>{calculateRemainingTime(new Date(`April ${day}, ${currentDate.getFullYear()} ${aprilSaharlikVaqtlar[index]}`), new Date(`April ${day}, ${currentDate.getFullYear()} ${aprilIftorlikVaqtlar[index]}`))}</td>
                             </tr>
                         ))}
                     </tbody>
